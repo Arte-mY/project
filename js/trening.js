e367c1dfde5576303e8bd2b78e7f684da11cec21
artsiom <artsiomyakusik@icloud.com>
2022-01-20 12:22:22 +0100

    lesson 28, 20.01.22

--- js/trening.js
+++ js/trening.js
@@ -101,3 +101,27 @@ let x = 5; alert(++x);
 // логический оператор || "И" останавливаеться на "првде" true 
 // ответ 2
 
+
+// lesson 28, 20.01.22  получение элементов со страницы
+// const box = document.getElementById('box');
+
+// console.log(box);
+
+// const btns = document.getElementsByTagName('button');
+
+// console.log(btns[0]);
+
+// const circles = document.getElementsByClassName('circle');
+
+// console.log(circles);
+
+// const hearts = document.querySelectorAll('.heart');
+
+// console.log(hearts);
+
+// hearts.forEach(item => {
+//   console.log(item);
+// });
+
+// const oneHeart = document.querySelector('.heart');
+// console.log(oneHeart);
